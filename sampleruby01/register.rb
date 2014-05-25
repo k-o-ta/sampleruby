025495637ed616aab9179592df499f0a3d85e1d7
Koji <afterjnih@gmail.com>
2014-05-25 16:37:07 +0900

    第一回コミットtolocal

---
+++ sampleruby01/register.rb
@@ -0,0 +1,22 @@
+#!/usr/local/bin/ruby
+
+require "cgi"
+
+print "Content-Type: text/html\n\n"
+print "<html><head></head><body>"
+
+c = CGI.new
+text1 = c["a"]
+text2 = c["b"]
+text3 = c["c"]
+text4 = c["d"]
+
+print text1
+print text2
+print text3
+print text4
+
+
+print "<br>"
+
+print "</body></html>"
